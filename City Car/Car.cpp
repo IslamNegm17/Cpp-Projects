a46089b4190abca2a86cf3a3939622ecc1efd4db
Islam Negm <77231946+IslamNegm17@users.noreply.github.com>
2023-02-11 00:01:31 +0200

    Create Car.cpp

---
+++ City Car/Car.cpp
@@ -0,0 +1,61 @@
+#include "Car.h"
+// ------------------------------------------------------
+// The methods of base class Car:
+Car::Car( long n, const string& prod)
+{
+    cout << "Creating an object of type Car." << endl;
+    nr = n; producer = prod;
+}
+Car::~Car()
+{
+    cout << "Destroying an object of type Car" << endl;
+}
+void Car::display() const
+{
+    cout << "\n---------------------------- "
+         << "\nCar number: " << nr
+         << "\nProducer: " << producer
+         << endl;
+}
+
+
+// -------------------------------------------------------
+// The methods of the derived class PassCar:
+PassCar::PassCar(const string& tp, bool sd, int n,const string& hs):Car( n, hs), passCarType( tp ), sunRoof( sd )
+{
+    cout << "I create an object of type PassCar." << endl;
+}
+PassCar::~PassCar()
+{
+    cout << "\nDestroying an object of type PassCar"
+         << endl;
+}
+void PassCar::display( void) const
+{
+    Car::display(); // Base class method
+    cout << "Type: " << passCarType
+         << "\nSunroof: ";
+    if(sunRoof)
+        cout << "yes "<< endl;
+    else
+        cout << "no " << endl;
+}
+
+
+// ----------------------------------------------------
+// The methods of the derived class Truck:
+Truck::Truck( int a, double t, int n, const string& hs)
+        : Car( n, hs), axles(a), tons(t)
+{
+    cout << "Creating an object of type Truck." << endl;
+}
+Truck::~Truck()
+{
+    cout << "\nDestroying an object of type Truck\n";
+}
+void Truck::display() const
+{
+    Car::display();
+    cout << "Axles: " << axles
+         << "\nCapacity: " << tons << " long tons\n";
+}
